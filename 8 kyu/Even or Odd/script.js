daa2fd89fccf39ecf405bc68e46d47f95a9b10df
joseluis1218 <joseluisml1218@gmail.com>
2022-05-08 20:07:39 -0600

    8 kyu - Even or Odd

---
+++ 8 kyu/Even or Odd/script.js
@@ -0,0 +1,13 @@
+/*
+    Title: Even or Odd
+
+    Kata's Sensei: suuuzi
+
+    Kata URL: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+    Description: Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+
+// Solution
+const even_or_odd = number => number % 2 === 0 ? 'Even' : 'Odd';
