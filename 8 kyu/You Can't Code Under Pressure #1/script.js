9a386f55cb23da21c5968e3647cb462c6f26c1cb
joseluis1218 <joseluisml1218@gmail.com>
2022-05-08 19:57:14 -0600

    8 kyu - You Can't Code Under Pressure #1

---
+++ 8 kyu/You Can't Code Under Pressure #1/script.js
@@ -0,0 +1,13 @@
+/*
+    Title: You Can't Code Under Pressure #1
+
+    Kata's Sensei: Kerrick
+
+    Kata URL: https://www.codewars.com/kata/53ee5429ba190077850011d4/javascript
+
+    Description: Code as fast as you can! You need to double the integer and return it.
+*/
+
+
+// Solution
+const doubleInteger = i => i*2;
