5b4c293067a3a11e79d0910a8d4ba7eea29f9d54
joseluis1218 <joseluisml1218@gmail.com>
2022-05-11 22:12:04 -0600

    8 kyu - Grasshopper - Debug sayHello

---
+++ 8 kyu/Grasshopper - Debug sayHello/script.js
@@ -0,0 +1,20 @@
+/*
+    Title: Grasshopper - Debug sayHello
+
+    Kata's Sensei: danleavitt0
+
+    Kata URL: https://www.codewars.com/kata/5625618b1fe21ab49f00001f
+
+    Description:Debugging sayHello function
+    The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+    Example output:
+
+    Hello, Mr. Spock
+*/
+
+// JavaScript
+// Solution
+function sayHello(name) {
+    return `Hello, ${name}`;
+}
