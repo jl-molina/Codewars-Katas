03bdac7a7b82dd58fc0dd0bc34fd2e086e4213bc
joseluis1218 <joseluisml1218@gmail.com>
2022-05-25 20:58:34 -0600

    8 kyu - Is it a palindrome?

---
+++ 8 kyu/Is it a palindrome/script.js
@@ -0,0 +1,16 @@
+/*
+    Title: Is it a palindrome?
+
+    Kata's Sensei: wichu
+
+    Kata URL: https://www.codewars.com/kata/57a1fd2ce298a731b20006a4
+
+    Description:Write a function that checks if a given string (case insensitive) is a palindrome.
+    */
+
+// PHP
+// Solution
+function isPalindrome(x) {
+  const arr = x.toUpperCase().split('');
+  return JSON.stringify(arr) === JSON.stringify(arr.reverse());
+}
